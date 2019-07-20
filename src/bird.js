cb08b99d7972b83c4c023dadfba9aecf97d1934a
Kaz Gofron <kgofron@comcast.net>
2019-07-20 19:00:11 -0400

    bird animation flicker

--- src/bird.js
+++ src/bird.js
@@ -1,7 +1,7 @@
 
 const CONSTANTS = {
     BIRD_WIDTH: 50,
-    BIRD_HEIGHT: 20,
+    BIRD_HEIGHT: 35,
     GRAVITY: 0.3,
     FLAP_VELOCITY: -8,
     TERMINAL_VEL: 12,
@@ -13,16 +13,42 @@ export default class Bird {
         this.pos_x = this.dimensions.width / 3;
         this.pos_y = this.dimensions.height / 2;
         this.velocity = 0;
+
+        const downFlap = new Image();
+        downFlap.src = "./images/bluebird-downflap.png";
+        const midFlap = new Image();
+        midFlap.src = "./images/bluebird-midflap.png";
+        const upFlap = new Image();
+        upFlap.src = "./images/bluebird-upflap.png";
+
+        this.currentLoopIdx = 0;
+        this.frameCount = 0;
+
+        this.birdAnimation = [upFlap, midFlap, downFlap];
     }
 
-    drawBird(ctx) {
-        ctx.fillStyle = "yellow";
-        ctx.fillRect(this.pos_x, this.pos_y, CONSTANTS.BIRD_WIDTH, CONSTANTS.BIRD_HEIGHT);
+    drawBird(ctx, birdImg) {
+        ctx.drawImage(birdImg, this.pos_x, this.pos_y, CONSTANTS.BIRD_WIDTH, CONSTANTS.BIRD_HEIGHT);
     }
+
+    // animate(ctx) {
+    //     this.drawBird(ctx, this.birdAnimation[0]);
+        
+    //     const pastFlap = this.birdAnimation.shift()
+    //     this.birdAnimation.push(pastFlap);
+    //     this.move();
+    // }
 
     animate(ctx) {
-        this.move();
-        this.drawBird(ctx);
+        this.frameCount += 1;
+
+        if(this.frameCount < 15) {
+            ctx.requestAnimationFrame(animate(ctx));
+            return;
+        }
+
+        this.frameCount = 0;
+        ctx.clearFrame()
     }
 
     move() {
